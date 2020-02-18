1a6c428f5fd01dbda16b5b355ddad865128869f0
Luiz <sacramento.tecnico@gmail.com>
2020-02-18 17:33:18 -0300

    ok

---
+++ Classe25-Trabalho/Arvore/lista.h
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct nodo {
+    int tVida,prioridade;
+    struct nodo *prox;
+}Nodo;
+
+Nodo *fim, *inicio=NULL;
+int eliminado=0;
+
+void inserir_lista(int t, int p){
+
+    Nodo *novo, *ant, *atual;
+    novo = (Nodo*)malloc(sizeof(Nodo));
+
+    if (novo == NULL) exit(1);
+    novo->tVida=t;
+    novo->prioridade=p;
+    novo -> prox  = NULL;
+
+    if(inicio == NULL){
+        inicio=novo;
+        fim = novo;
+        ant = novo;
+    } else{
+            fim -> prox = novo;
+            fim = novo;
+    }
+}
+
+void dimiuirTLL(int fila){
+   Nodo *aux;
+   aux=inicio;
+
+   if(fila==1){
+      aux->tVida--;
+      if(aux->tVida==0){
+           free(aux);
+           eliminado++;
+      }
+   }else if(fila==2){
+             aux->tVida;
+             aux->prox;
+             aux->tVida--;
+             if(aux->tVida==0){
+                free(aux);
+                eliminado++;
+             }
+          }else if(fila==3){
+                    aux->tVida;
+                    aux->prox;
+                    aux->prox;
+                    aux->tVida--;
+                    if(aux->tVida==0){
+                      free(aux);
+                      eliminado++;
+                    }
+                 }else{
+                        aux = fim;
+                        aux->tVida--;
+                        if(aux->tVida==0){
+                          free(aux);
+                          eliminado++;
+
+                        }
+                       }
+}
+
---
+++ Classe25-Trabalho/Arvore/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+#include <time.h>
+
+void main()
+{
+    srand(time(NULL));
+
+    int qtd=3;
+    for (int i = 0; i < qtd; i++) {
+      escolha((rand() % 10)+1,(rand() % 10)+1);
+    }
+    imprimirPacotesIO();
+    system("pause");
+
+}
+
+
+
+
+
+
